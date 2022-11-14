0f96f132759433cf2d2da05c3b7cda5aecbd861e
toast <58362380+Traumatism@users.noreply.github.com>
2022-11-14 13:02:37 +0100

    Update

--- src/parse.rs
+++ src/parse.rs
@@ -5,11 +5,28 @@ use crate::vm::MaeelMachine;
 pub fn parse(tokens: &mut Stack<Token>, vm: &mut MaeelMachine<isize>) {
     while let Some(token) = tokens.pop() {
         match token {
-            Token::Operator(operator) => match operator {
-                Operator::Plus => vm.add(),
-                Operator::Minus => vm.sub(),
-            },
-            Token::Number(number) => vm.push(number),
+            Token::Operator(operator) => {
+                let (a, b) = match (
+                    tokens
+                        .pop()
+                        .unwrap_or_else(|| Token::Number(vm.pop().expect("Missing `a`"))),
+                    tokens
+                        .pop()
+                        .unwrap_or_else(|| Token::Number(vm.pop().expect("Missing `b`"))),
+                ) {
+                    (Token::Number(a), Token::Number(b)) => (a, b),
+                    _ => panic!("`a` and `b` must be integers"),
+                };
+
+                vm.push(a);
+                vm.push(b);
+
+                match operator {
+                    Operator::Plus => vm.add(),
+                    Operator::Minus => vm.sub(),
+                }
+            }
+            Token::Number(value) => vm.push(value),
             _ => panic!(),
         }
     }
