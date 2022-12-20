a1de020053ac01ee44d01604f5704b5d3fa6ca43
toast <58362380+Traumatism@users.noreply.github.com>
2022-12-20 20:37:14 +0100

    Update

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,32 @@ mod tokenize;
 mod vm;
 
 fn main() {
-    let mut playground = playground::Playground::default();
+    let args = std::env::args().collect::<Vec<String>>();
 
-    loop {
-        let mut user_input = String::new();
+    if args.len() == 1 {
+        let mut playground = playground::Playground::default();
 
-        stdout().write_all("-> ".as_bytes()).unwrap();
-        stdout().flush().unwrap();
-        stdin().read_line(&mut user_input).unwrap();
+        loop {
+            let mut user_input = String::new();
 
-        playground.evaluate_expression(user_input);
+            stdout().write_all(">>> ".as_bytes()).unwrap();
+            stdout().flush().unwrap();
+            stdin().read_line(&mut user_input).unwrap();
 
-        println!("\n<- {:?}", playground.get_output())
+            playground.evaluate_expression(user_input);
+
+            println!("\nCurrent stack: {:?}", playground.get_output())
+        }
+    }
+
+    let content = std::fs::read_to_string(args.get(1).unwrap()).expect("Failed to open file");
+
+    let mut vm = vm::Stack::<enums::VMType>::default();
+
+    let mut instructions =
+        tokenize::parse_into_instructions(&mut tokenize::lex_into_tokens(&content));
+
+    while let Some(mut instruction) = instructions.pop() {
+        parse::parse(&mut instruction, &mut vm)
     }
 }
