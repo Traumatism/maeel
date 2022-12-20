859196d472af01b96d711bb03c9f325166d4645f
toast <58362380+Traumatism@users.noreply.github.com>
2022-12-20 17:17:11 +0100

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,7 @@
 pub mod enums;
+pub mod optimization;
+pub mod parse;
+pub mod playground;
+pub mod tokenize;
+pub mod utils;
 pub mod vm;
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,32 @@
 mod enums;
 mod optimization;
 mod parse;
+mod playground;
 mod tokenize;
 mod utils;
 mod vm;
 
+fn _playground() {
+    let mut pg = playground::Playground::default();
+
+    pg.evaluate_expression("push 1 2 3");
+    println!("{:?}", pg.get_output());
+
+    pg.evaluate_expression("pop");
+    println!("{:?}", pg.get_output());
+}
+
 fn main() {
     let args = std::env::args().collect::<Vec<String>>();
-    let content = std::fs::read_to_string(args.get(1).unwrap()).expect("Failed to open file");
+    let first = args.get(1).unwrap();
+
+    let content = std::fs::read_to_string(first).expect("Failed to open file");
 
     let mut vm = vm::Stack::<enums::VMType>::default();
+
     let mut instructions =
         tokenize::parse_into_instructions(&mut tokenize::lex_into_tokens(&content));
+
     while let Some(mut instruction) = instructions.pop() {
         parse::parse(&mut instruction, &mut vm)
     }
