488160e06e453a1c9a5483b4c71474e18d5af993
toast <58362380+Traumatism@users.noreply.github.com>
2022-12-20 11:57:00 +0100

    Update

--- src/enums.rs
+++ src/enums.rs
@@ -2,15 +2,15 @@
 pub enum Token {
     Separator,
     String(String, u16),
-    Integer(isize, u16),
+    Integer(i64, u16),
     Float(f64, u16),
     Identifier(String, u16),
 }
 
 #[derive(Debug)]
 pub enum VMType {
-    Integer(isize),
     Float(f64),
+    Integer(i64),
     String(String),
     Array(Vec<VMType>),
 }
