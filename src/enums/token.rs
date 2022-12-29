48a351df4059d4a8d7b8e17632edc2aa14e54ab0
toast <58362380+Traumatism@users.noreply.github.com>
2022-12-29 14:45:03 +0100

    Update

--- src/enums/token.rs
+++ src/enums/token.rs
@@ -1,33 +1,34 @@
 /// All tokens lexer/parser can understand
 #[derive(Debug, Clone)]
 pub enum Token {
-    Str(String, u16),        // "hello world"
-    Integer(i64, u16),       // 123
-    Identifier(String, u16), // helloworld
-    Float(f64, u16),         // 123.4
-    Bool(bool, u16),         // true|false
-    Let(u16),                // let
-    If(u16),                 // if
-    For(u16),                // for
-    Modulo(u16),             // %
-    Add(u16),                // +
-    Sub(u16),                // -
-    Mul(u16),                // *
-    Div(u16),                // /
-    And(u16),                // &
-    Or(u16),                 // |
-    Xor(u16),                // ^
-    Not(u16),                // !
-    Eq(u16),                 // =
-    Del(u16),                // del
-    Take(u16),               // take
-    Return(u16),             // return
-    Reverse(u16),            // reverse
-    Clear,                   // clear
-    Dup,                     // dup
-    Swap,                    // swap
-    Pop,                     // pop
-    Separator,               // end
-    ProcStart,               // proc
-    ProcEnd,                 // proc_end
+    Str(String),        // "hello world"
+    Integer(i64),       // 123
+    Identifier(String), // helloworld
+    Float(f64),         // 123.4
+    Bool(bool),         // true|false
+    Let,                // let
+    If,                 // if
+    For,                // for
+    Modulo,             // %
+    Add,                // +
+    Sub,                // -
+    Mul,                // *
+    Div,                // /
+    And,                // &
+    Or,                 // |
+    Xor,                // ^
+    Not,                // !
+    Eq,                 // =
+    Del,                // del
+    Take,               // take
+    Reverse,            // reverse
+    Clear,              // clear
+    Dup,                // dup
+    Swap,               // swap
+    Pop,                // pop
+    Return,             // return
+    ProcStart,          // proc
+    BlockStart,
+    BlockEnd,
+    Block(Vec<Token>),
 }
