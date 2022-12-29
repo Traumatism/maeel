7b3892bc8c50dab2b8a48aa51522966c87b18992
toast <58362380+Traumatism@users.noreply.github.com>
2022-12-29 15:09:15 +0100

    Update

--- src/enums/token.rs
+++ src/enums/token.rs
@@ -1,34 +1,34 @@
 /// All tokens lexer/parser can understand
 #[derive(Debug, Clone)]
 pub enum Token {
-    Str(String),        // "hello world"
-    Integer(i64),       // 123
-    Identifier(String), // helloworld
-    Float(f64),         // 123.4
-    Bool(bool),         // true|false
-    Let,                // let
-    If,                 // if
-    For,                // for
-    Modulo,             // %
-    Add,                // +
-    Sub,                // -
-    Mul,                // *
-    Div,                // /
-    And,                // &
-    Or,                 // |
-    Xor,                // ^
-    Not,                // !
-    Eq,                 // =
-    Del,                // del
-    Take,               // take
-    Reverse,            // reverse
-    Clear,              // clear
-    Dup,                // dup
-    Swap,               // swap
-    Pop,                // pop
-    Return,             // return
-    ProcStart,          // proc
-    BlockStart,
-    BlockEnd,
-    Block(Vec<Token>),
+    Str(String, u16),        // "hello world"
+    Integer(i64, u16),       // 123
+    Identifier(String, u16), // helloworld
+    Float(f64, u16),         // 123.4
+    Bool(bool, u16),         // true|false
+    Let(u16),                // let
+    If(u16),                 // if
+    For(u16),                // for
+    Modulo(u16),             // %
+    Add(u16),                // +
+    Sub(u16),                // -
+    Mul(u16),                // *
+    Div(u16),                // /
+    And(u16),                // &
+    Or(u16),                 // |
+    Xor(u16),                // ^
+    Not(u16),                // !
+    Eq(u16),                 // =
+    Del(u16),                // del
+    Take(u16),               // take
+    Reverse(u16),            // reverse
+    Clear(u16),              // clear
+    Dup(u16),                // dup
+    Swap(u16),               // swap
+    Pop(u16),                // pop
+    Return(u16),             // return
+    ProcStart(u16),          // proc
+    BlockStart(u16),
+    BlockEnd(u16),
+    Block(Vec<Token>, u16),
 }
--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -7,8 +7,8 @@ pub fn extract_instructions(tokens: Vec<Token>) -> Vec<Vec<Token>> {
 
     for next in tokens {
         match next.clone() {
-            Token::BlockEnd => {
-                current_instruction.push(Token::BlockEnd);
+            Token::BlockEnd(line) => {
+                current_instruction.push(Token::BlockEnd(line));
                 instructions.push(current_instruction);
                 current_instruction = Vec::default()
             }
@@ -27,21 +27,21 @@ pub fn extract_blocks(tokens: Vec<Token>) -> Vec<Token> {
 
     while let Some(token) = tokens_iter.next() {
         match token {
-            Token::BlockStart => {
+            Token::BlockStart(line) => {
                 let mut block_tokens = Vec::new();
                 let mut need_recursion = false;
                 let mut n = 0;
 
                 for token_0 in tokens_iter.by_ref() {
                     match token_0 {
-                        Token::BlockEnd => match n {
+                        Token::BlockEnd(_) => match n {
                             0 => break,
                             _ => {
                                 n -= 1;
                                 block_tokens.push(token_0.clone())
                             }
                         },
-                        Token::BlockStart => {
+                        Token::BlockStart(_) => {
                             n += 1;
                             need_recursion = true;
                             block_tokens.push(token_0.clone())
@@ -51,8 +51,8 @@ pub fn extract_blocks(tokens: Vec<Token>) -> Vec<Token> {
                 }
 
                 match need_recursion {
-                    true => output.push(Token::Block(extract_blocks(block_tokens))),
-                    false => output.push(Token::Block(block_tokens)),
+                    true => output.push(Token::Block(extract_blocks(block_tokens), *line)),
+                    false => output.push(Token::Block(block_tokens, *line)),
                 }
             }
             _ => output.push(token.clone()),
@@ -63,64 +63,64 @@ pub fn extract_blocks(tokens: Vec<Token>) -> Vec<Token> {
 }
 
 /// Lex an identifier
-pub fn lex_identifier(identifier: &str) -> Token {
+pub fn lex_identifier(identifier: &str, line: u16) -> Token {
     let mut keywords = HashMap::new();
 
     // Booleans values
-    keywords.insert("true", Token::Bool(true));
-    keywords.insert("false", Token::Bool(false));
+    keywords.insert("true", Token::Bool(true, line));
+    keywords.insert("false", Token::Bool(false, line));
 
     // Boolean operations
-    keywords.insert("or", Token::Or);
-    keywords.insert("and", Token::And);
-    keywords.insert("not", Token::Not);
-    keywords.insert("xor", Token::Xor);
-    keywords.insert("eq", Token::Eq);
+    keywords.insert("or", Token::Or(line));
+    keywords.insert("and", Token::And(line));
+    keywords.insert("not", Token::Not(line));
+    keywords.insert("xor", Token::Xor(line));
+    keywords.insert("eq", Token::Eq(line));
 
     // Block borders
-    keywords.insert("do", Token::BlockStart);
-    keywords.insert("end", Token::BlockEnd);
+    keywords.insert("do", Token::BlockStart(line));
+    keywords.insert("end", Token::BlockEnd(line));
 
-    keywords.insert("proc", Token::ProcStart);
+    keywords.insert("proc", Token::ProcStart(line));
 
     // Stack manipulation
-    keywords.insert("dup", Token::Dup);
-    keywords.insert("pop", Token::Pop);
-    keywords.insert("clear", Token::Clear);
-    keywords.insert("swap", Token::Swap);
+    keywords.insert("dup", Token::Dup(line));
+    keywords.insert("pop", Token::Pop(line));
+    keywords.insert("clear", Token::Clear(line));
+    keywords.insert("swap", Token::Swap(line));
 
     // Array manipulation
-    keywords.insert("reverse", Token::Reverse);
-    keywords.insert("take", Token::Take);
+    keywords.insert("reverse", Token::Reverse(line));
+    keywords.insert("take", Token::Take(line));
 
     // Statements
-    keywords.insert("for", Token::For);
-    keywords.insert("del", Token::Del);
-    keywords.insert("if", Token::If);
-    keywords.insert("let", Token::Let);
-    keywords.insert("return", Token::Return);
+    keywords.insert("for", Token::For(line));
+    keywords.insert("del", Token::Del(line));
+    keywords.insert("if", Token::If(line));
+    keywords.insert("let", Token::Let(line));
+    keywords.insert("return", Token::Return(line));
 
     let token = keywords.get(identifier);
 
     token
-        .unwrap_or(&Token::Identifier(String::from(identifier)))
+        .unwrap_or(&Token::Identifier(String::from(identifier), line))
         .clone()
 }
 
 /// Lex a single character
-pub fn lex_single_char(chr: char) -> Token {
+pub fn lex_single_char(chr: char, line: u16) -> Token {
     match chr {
-        '&' => Token::And,
-        '|' => Token::Or,
-        '^' => Token::Xor,
-        '+' => Token::Add,
-        '*' => Token::Mul,
-        '/' => Token::Div,
-        '-' => Token::Sub,
-        '=' => Token::Eq,
-        '%' => Token::Modulo,
-        '!' => Token::Not,
-        _ => panic!(),
+        '&' => Token::And(line),
+        '|' => Token::Or(line),
+        '^' => Token::Xor(line),
+        '+' => Token::Add(line),
+        '*' => Token::Mul(line),
+        '/' => Token::Div(line),
+        '-' => Token::Sub(line),
+        '=' => Token::Eq(line),
+        '%' => Token::Modulo(line),
+        '!' => Token::Not(line),
+        _ => panic!("line {line}: Unkown symbol `{chr}`"),
     }
 }
 
@@ -130,10 +130,14 @@ pub fn lex_into_tokens(code: &str) -> Vec<Token> {
     chars.reverse();
 
     let mut tokens = Vec::new();
+    let mut line = 1;
 
     while let Some(chr) = chars.pop() {
         match chr {
-            ' ' | '\n' | '(' | ')' => (),
+            ' ' | '(' | ')' => (),
+
+            '\n' => line += 1,
+
             '@' => {
                 while let Some(next) = chars.pop() {
                     if next == '\n' {
@@ -151,7 +155,7 @@ pub fn lex_into_tokens(code: &str) -> Vec<Token> {
                     }
                 }
 
-                tokens.push(Token::Str(content))
+                tokens.push(Token::Str(content, line))
             }
             'a'..='z' | '_' => {
                 let mut content = String::from(chr);
@@ -166,7 +170,7 @@ pub fn lex_into_tokens(code: &str) -> Vec<Token> {
                     }
                 }
 
-                tokens.push(lex_identifier(&content).clone());
+                tokens.push(lex_identifier(&content, line).clone());
             }
             '0'..='9' => {
                 let mut content = String::from(chr);
@@ -188,11 +192,21 @@ pub fn lex_into_tokens(code: &str) -> Vec<Token> {
                 }
 
                 tokens.push(match float {
-                    true => Token::Float(content.parse::<f64>().unwrap()),
-                    false => Token::Integer(content.parse::<i64>().unwrap()),
+                    true => Token::Float(
+                        content.parse::<f64>().unwrap_or_else(|_| {
+                            panic!("line {line}: Failed to parse `{content}` into a float")
+                        }),
+                        line,
+                    ),
+                    false => Token::Integer(
+                        content.parse::<i64>().unwrap_or_else(|_| {
+                            panic!("line {line}: Failed to parse `{content}` into an integer")
+                        }),
+                        line,
+                    ),
                 });
             }
-            _ => tokens.push(lex_single_char(chr)),
+            _ => tokens.push(lex_single_char(chr, line)),
         }
     }
 
