63fd385fc3b5d37e33510229b63577b0ea23507f
toast <58362380+Traumatism@users.noreply.github.com>
2023-05-01 00:07:12 +0200

    Updated linting rules

--- maeel_lexer/src/main.rs
+++ maeel_lexer/src/main.rs
@@ -1,14 +1,11 @@
 use maeel_lexer::lex_into_tokens;
 
 use std::env::args;
-
 use std::fs::read_to_string;
-
 use std::io::Result;
 
 fn main() -> Result<()>
 {
-
     let content = read_to_string(
         args()
             .collect::<Vec<String>>()
