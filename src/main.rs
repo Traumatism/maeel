4b8f9e1a47301f9492cf5d18272843f352f8c81c
toast <58362380+Traumatism@users.noreply.github.com>
2023-05-22 12:32:37 +0200

    Improved stdlib performances

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,27 @@ pub enum VMType {
 
 impl std::fmt::Display for VMType {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        /*
+        TODO: implement a function to check the type of the top element
+        of the array. Should make function overloading (like print) easier.
+        So their could be a print function for integer, a print function for strings
+        etc.
+        */
+
         match self {
-            VMType::Function(tokens) => write!(f, "{:?}", tokens),
-            VMType::Float(x) => write!(f, "{}", x),
-            VMType::Integer(x) => write!(f, "{}", x),
+            // Write a string to stdout (impossible to implement in maeel, by design)
             VMType::String(x) => write!(f, "{}", x),
+
+            // Write an anonymous function to stdout (impossible to implement in maeel)
+            VMType::Function(tokens) => write!(f, "{:?}", tokens),
+
+            // Write a float to stdout (float2str[maeel] is in dev)
+            VMType::Float(x) => write!(f, "{}", x),
+
+            // Write an integer to stdout (int2str[maeel] is in dev)
+            VMType::Integer(x) => write!(f, "{}", x),
+
+            // Write an array to stdout
             VMType::Array(xs) => {
                 write!(f, "{{")?;
 
@@ -69,10 +85,16 @@ impl std::ops::Sub for VMType {
 
     fn sub(self, rhs: Self) -> Self::Output {
         match (self, rhs) {
-            (VMType::Integer(a), VMType::Integer(b)) => VMType::Integer(a - b),
-            (VMType::Float(a), VMType::Float(b)) => VMType::Float(a - b),
-            (VMType::Integer(a), VMType::Float(b)) => VMType::Float(a as f64 - b),
-            (VMType::Float(a), VMType::Integer(b)) => VMType::Float(a - b as f64),
+            // int - int
+            (VMType::Integer(m), VMType::Integer(n)) => VMType::Integer(m - n),
+
+            // float - float
+            (VMType::Float(x), VMType::Float(y)) => VMType::Float(x - y),
+
+            // int - float
+            (VMType::Float(x), VMType::Integer(m)) | (VMType::Integer(m), VMType::Float(x)) => {
+                VMType::Float(m as f64 - x)
+            }
 
             (a, b) => panic!("Cannot substract {a} and {b}"),
         }
@@ -84,10 +106,15 @@ impl std::ops::Mul for VMType {
 
     fn mul(self, rhs: Self) -> Self::Output {
         match (self, rhs) {
-            (VMType::Integer(a), VMType::Integer(b)) => VMType::Integer(a * b),
-            (VMType::Float(a), VMType::Float(b)) => VMType::Float(a * b),
-            (VMType::Float(b), VMType::Integer(a)) | (VMType::Integer(a), VMType::Float(b)) => {
-                VMType::Float(b * a as f64)
+            // int * int
+            (VMType::Integer(m), VMType::Integer(n)) => VMType::Integer(m * n),
+
+            // float * float
+            (VMType::Float(x), VMType::Float(y)) => VMType::Float(x * y),
+
+            // float * float
+            (VMType::Float(x), VMType::Integer(m)) | (VMType::Integer(m), VMType::Float(x)) => {
+                VMType::Float(x * m as f64)
             }
 
             (a, b) => panic!("Cannot multiply {a} and {b}"),
@@ -100,11 +127,21 @@ impl std::ops::Add for VMType {
 
     fn add(self, rhs: Self) -> Self::Output {
         match (self, rhs) {
+            // str + str
             (VMType::String(a), VMType::String(b)) => VMType::String(a + &b),
-            (VMType::Integer(a), VMType::Integer(b)) => VMType::Integer(a + b),
-            (VMType::Float(a), VMType::Float(b)) => VMType::Float(a + b),
-            (VMType::Integer(a), VMType::Float(b)) => VMType::Float(a as f64 + b),
-            (VMType::Float(a), VMType::Integer(b)) => VMType::Float(a + b as f64),
+
+            // int + int
+            (VMType::Integer(m), VMType::Integer(n)) => VMType::Integer(m + n),
+
+            // float + float
+            (VMType::Float(x), VMType::Float(y)) => VMType::Float(x + y),
+
+            // int + float
+            (VMType::Integer(m), VMType::Float(x)) | (VMType::Float(x), VMType::Integer(m)) => {
+                VMType::Float(m as f64 + x)
+            }
+
+            // array + e and e + array
             (other, VMType::Array(mut xs)) | (VMType::Array(mut xs), other) => {
                 xs.push(other);
                 VMType::Array(xs)
@@ -120,10 +157,17 @@ impl std::ops::Rem for VMType {
 
     fn rem(self, rhs: Self) -> Self::Output {
         match (self, rhs) {
-            (VMType::Integer(a), VMType::Integer(b)) => VMType::Integer(a % b),
-            (VMType::Float(a), VMType::Float(b)) => VMType::Float(a % b),
-            (VMType::Integer(a), VMType::Float(b)) => VMType::Float(a as f64 % b),
-            (VMType::Float(a), VMType::Integer(b)) => VMType::Float(a % b as f64),
+            // int % int
+            (VMType::Integer(m), VMType::Integer(n)) => VMType::Integer(m % n),
+
+            // float % float
+            (VMType::Float(x), VMType::Float(y)) => VMType::Float(x % y),
+
+            // int % float
+            (VMType::Integer(m), VMType::Float(x)) => VMType::Float(m as f64 % x),
+
+            // float % int
+            (VMType::Float(x), VMType::Integer(m)) => VMType::Float(x % m as f64),
 
             (a, b) => panic!("Cannot divide {a} and {b}"),
         }
@@ -135,10 +179,17 @@ impl std::ops::Div for VMType {
 
     fn div(self, rhs: Self) -> Self::Output {
         match (self, rhs) {
-            (VMType::Integer(a), VMType::Integer(b)) => VMType::Float(a as f64 / b as f64),
-            (VMType::Float(a), VMType::Float(b)) => VMType::Float(a / b),
-            (VMType::Integer(a), VMType::Float(b)) => VMType::Float(a as f64 / b),
-            (VMType::Float(a), VMType::Integer(b)) => VMType::Float(a / b as f64),
+            // int / int
+            (VMType::Integer(m), VMType::Integer(n)) => VMType::Float(m as f64 / n as f64),
+
+            // float / float
+            (VMType::Float(x), VMType::Float(y)) => VMType::Float(x / y),
+
+            // int / float
+            (VMType::Integer(m), VMType::Float(x)) => VMType::Float(m as f64 / x),
+
+            // float / int
+            (VMType::Float(x), VMType::Integer(m)) => VMType::Float(x / m as f64),
 
             (a, b) => panic!("Cannot divide {a} and {b}"),
         }
