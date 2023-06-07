014f46d13bbf0eccc4a154d47d187ff613802e9d
toast <58362380+Traumatism@users.noreply.github.com>
2023-06-07 05:59:24 +0200

    Updated Ikuyo VM

--- src/vm/ikuyo.rs
+++ src/vm/ikuyo.rs
@@ -1,15 +1,15 @@
 use super::*;
 
 pub struct IkuyoVM<T, const MAX_SIZE: usize> {
-    buffer: [T; MAX_SIZE],
-    top: usize,
+    buffer: [T; MAX_SIZE], /* Main stack buffer */
+    sp: usize,             /* Stack pointer */
 }
 
 impl<T, const MAX_SIZE: usize> Default for IkuyoVM<T, MAX_SIZE> {
     fn default() -> IkuyoVM<T, MAX_SIZE> {
         IkuyoVM {
             buffer: unsafe { std::mem::zeroed() },
-            top: 0,
+            sp: 0,
         }
     }
 }
@@ -18,45 +18,41 @@ impl<T: Clone, const MAX_SIZE: usize> MaeelVM for IkuyoVM<T, MAX_SIZE> {
     type Data = T;
 
     fn push(&mut self, item: T) -> VMOutput<()> {
-        assert!(self.top < self.buffer.len(), "Stack overflow");
+        /* Making sure the stack is not filled */
+        assert!(self.sp < self.buffer.len(), "Stack overflow");
 
-        unsafe {
-            ptr::write(self.buffer.as_mut_ptr().add(self.top), item);
-        }
+        unsafe { ptr::write(self.buffer.as_mut_ptr().add(self.sp), item) }
 
-        self.top += 1;
+        self.sp += 1; /* Increase stack pointer */
 
         Ok(())
     }
 
     fn pop(&mut self) -> VMOutput<T> {
-        if self.top == 0 {
-            Err("Stack is empty".into())
-        } else {
-            self.top -= 1;
+        /* Making sure the stack is not empty */
+        assert!(self.sp > 0, "Stack is empty");
 
-            unsafe { Ok(ptr::read(self.buffer.as_ptr().add(self.top))) }
-        }
+        self.sp -= 1; /* Decrease stack pointer */
+
+        unsafe { Ok(ptr::read(self.buffer.as_ptr().add(self.sp))) }
     }
 
     fn dup(&mut self) -> VMOutput<()> {
-        if let Ok(item) = self.peek() {
-            self.push(item.clone())?;
-        }
+        /* Making sure the stack is not empty */
+        assert!(self.sp > 0, "Stack is empty");
 
-        Ok(())
+        self.push(self.peek()?.clone())
     }
 
     fn swap(&mut self) -> VMOutput<()> {
-        if self.top < 2 {
-            return Err("Not enough elements on the stack".into());
-        }
+        /* Making sure the stack contains at least two values */
+        assert!(self.sp > 1, "Stack has less than two elements");
 
         unsafe {
             ptr::swap(
-                self.buffer.as_mut_ptr().add(self.top - 1),
-                self.buffer.as_mut_ptr().add(self.top - 2),
-            );
+                self.buffer.as_mut_ptr().add(self.sp - 1),
+                self.buffer.as_mut_ptr().add(self.sp - 2),
+            )
         }
 
         Ok(())
@@ -69,42 +65,41 @@ impl<T: Clone, const MAX_SIZE: usize> MaeelVM for IkuyoVM<T, MAX_SIZE> {
     }
 
     fn clear(&mut self) -> VMOutput<()> {
-        self.top = 0;
+        self.sp = 0;
 
         Ok(())
     }
 
     fn peek(&self) -> VMOutput<&T> {
-        if self.top == 0 {
-            Err("Stack is empty".into())
-        } else {
-            unsafe { Ok(&*self.buffer.as_ptr().add(self.top - 1)) }
-        }
+        /* Making sure the stack is not empty */
+        assert!(self.sp > 0, "Stack is empty");
+
+        unsafe { Ok(&*self.buffer.as_ptr().add(self.sp - 1)) }
     }
 
     fn rot(&mut self) -> VMOutput<()> {
-        if self.top >= 3 {
-            unsafe {
-                let node1 = self.buffer.as_mut_ptr().add(self.top - 1);
-                let node2 = self.buffer.as_mut_ptr().add(self.top - 2);
-                let node3 = self.buffer.as_mut_ptr().add(self.top - 3);
+        /* Making sure the stack contains at least three values */
+        assert!(self.sp > 2, "Stack has less than three elements");
 
-                let temp = ptr::read(node1);
+        unsafe {
+            let node1 = self.buffer.as_mut_ptr().add(self.sp - 1);
+            let node2 = self.buffer.as_mut_ptr().add(self.sp - 2);
+            let node3 = self.buffer.as_mut_ptr().add(self.sp - 3);
 
-                ptr::write(node1, ptr::read(node2));
-                ptr::write(node2, ptr::read(node3));
-                ptr::write(node3, temp);
-            }
+            let temp = ptr::read(node1);
+
+            ptr::write(node1, ptr::read(node2));
+            ptr::write(node2, ptr::read(node3));
+            ptr::write(node3, temp)
         }
 
         Ok(())
     }
 
     fn over(&mut self) -> VMOutput<()> {
-        if self.top >= 2 {
-            self.push(unsafe { ptr::read(self.buffer.as_ptr().add(self.top - 2)) })?;
-        }
+        /* Making sure the stack contains at least two values */
+        assert!(self.sp > 1, "Stack has less than two elements");
 
-        Ok(())
+        self.push(unsafe { ptr::read(self.buffer.as_ptr().add(self.sp - 2)) })
     }
 }
