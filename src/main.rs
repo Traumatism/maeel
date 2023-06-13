c2fd556b5dd20980e5a7bca3166c8eb2b7982680
toast <58362380+Traumatism@users.noreply.github.com>
2023-06-13 11:43:23 +0200

    Bunch of optimizations and project structure refactoring

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,11 @@ use std::env::args;
 use std::fs::read_to_string;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    #[cfg(any(not(maeelvm = "ikuyo"), maeelvm = "bocchi"))]
+    #[cfg(any(not(maeelvm = "nijika"), maeelvm = "bocchi"))]
     let mut vm = vm::BocchiVM::default();
 
-    #[cfg(all(maeelvm = "ikuyo"))]
-    let mut vm = vm::IkuyoVM::<32>::default();
+    #[cfg(all(maeelvm = "nijika"))]
+    let mut vm = vm::NijikaVM::<32>::default();
 
     vm.process_tokens(
         &lex_into_tokens(&read_to_string(args().nth(1).unwrap())?),
