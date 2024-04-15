7346e52c577f391f2c3324d20b8e58638238d4e3
b2o <b2oba@dnmx.org>
2024-04-15 06:50:17 +0200

    Little commit

--- maeelc.rs
+++ maeelc.rs
@@ -3,6 +3,7 @@ This is STILL in development phase !! (partially works)
 */
 
 use std::fs::File;
+use std::collections::HashMap;
 use std::io::Read;
 
 mod maeellex;
@@ -29,117 +30,106 @@ enum Instruction {
     Push(M_INT_SIZE),
     BuiltInCall(BuiltIn),
     PushAsm(String),
+    Store(String),
+    Load(String)
 }
 
 fn parse_tokens(
     tokens: &mut Vec<TokenData>,
     rev: bool,
-) -> Vec<Instruction> {
+) {
     if rev /* Sometimes we might act like the tokens vec was a stack */ { tokens.reverse(); }
 
     let mut instructions = Vec::new();
-
+    let mut symbol_table: HashMap<String, i32> = HashMap::new();
+    let mut stack_pointer = 0; // Initialize stack pointer
     while let Some((token, file, line)) = tokens.pop() {
         match token {
             | Token::Sym(M_ADD!()) => instructions.push(Instruction::BinOp(BinOp::Add)),
             | Token::Sym(M_SUB!()) => instructions.push(Instruction::BinOp(BinOp::Sub)),
             | Token::Sym(M_MUL!()) => instructions.push(Instruction::BinOp(BinOp::Mul)),
             | Token::Int(value) => instructions.push(Instruction::Push(value)),
+            | Token::Sym('~') => {
+                let name = expect_token!(Name, tokens, file, line);
+                instructions.push(Instruction::Store(name.clone()));
+                symbol_table.insert(name, stack_pointer);
+                stack_pointer += 64;
+            }
             | Token::Sym('$') => {
                 let data = expect_token!(Str, tokens, file, line);
                 instructions.push(Instruction::PushAsm(data));
             }
-            | Token::Name(name) => match name.as_str() {
-                | M_PUTS!() => instructions.push(Instruction::BuiltInCall(BuiltIn::Puts)), 
-                | _ => unreachable!()
+            | Token::Name(name) => {
+                if let Some(&pos) = symbol_table.get(&name) {
+                    instructions.push(Instruction::Load(name.clone()));
+                } else {
+                    match name.as_str() {
+                        | M_PUTS!() => instructions.push(Instruction::BuiltInCall(BuiltIn::Puts)),
+                        | _ => unreachable!()
+                    }
+                }
             }
             | _ => unreachable!()
         }
     }
 
-    instructions
-}
-
-fn generate_asm(instructions: Vec<Instruction>) {
-    let mut output = String::new();
-
-    output.push_str("segment .text\n");
-    output.push_str("print_integer:\n");
-    output.push_str("    mov     r9, -3689348814741910323\n");
-    output.push_str("    sub     rsp, 40\n");
-    output.push_str("    mov     BYTE [rsp+31], 10\n");
-    output.push_str("    lea     rcx, [rsp+30]\n");
-    output.push_str("    .L2:                               ;; iterate through each digit\n");
-    output.push_str("    mov     rax, rdi                   ;; rax <- rdi\n");
-    output.push_str("    lea     r8, [rsp+32]               ;; r8 <- rsp+32\n");
-    output.push_str("    mul     r9                         ;; rax <- rax * r9\n");
-    output.push_str("    mov     rax, rdi                   ;; rax <- rdi\n");
-    output.push_str("    sub     r8, rcx                    ;; r8 <- r8 - rcx\n");
-    output.push_str("    shr     rdx, 3                     ;; right shift (3 bits)\n");
-    output.push_str("    lea     rsi, [rdx*5]               ;; rsi <- 5*rdx\n");
-    output.push_str("    add     rsi, rsi                   ;; rsi <- 2*rsi\n");
-    output.push_str("    sub     rax, rsi                   ;; rax <- rsi\n");
-    output.push_str("    add     eax, 48                    ;; eax <- eax + 48\n");
-    output.push_str("    mov     BYTE [rcx], al             ;;\n");
-    output.push_str("    mov     rax, rdi                   ;; rax <- rdi\n");
-    output.push_str("    mov     rdi, rdx                   ;; rdi <- rdx\n");
-    output.push_str("    mov     rdx, rcx                   ;; rdx <- rcx\n");
-    output.push_str("    dec     rcx                        ;; rcx <- rcx - 1\n");
-    output.push_str("    cmp     rax, 9                     ;; if rax < 9\n");
-    output.push_str("    ja      .L2                        ;;    recursive call\n");
-    output.push_str("    lea     rax, [rsp+32]              ;; rax <- [rsp+32]\n");
-    output.push_str("    mov     edi, 1                     ;; edi <- 1\n");
-    output.push_str("    sub     rdx, rax                   ;; rdx <- rdx - rax (>= 9)\n");
-    output.push_str("    xor     eax, eax                   ;; clear eax\n");
-    output.push_str("    mov     rax, 1                     ;; write(\n");
-    output.push_str("    lea     rsi, [rsp+32+rdx]          ;;  integer as a string,\n");
-    output.push_str("    mov     rdx, r8                    ;;  char count\n");
-    output.push_str("    syscall                            ;; );\n");
-    output.push_str("    add     rsp, 40\n");
-    output.push_str("    ret\n");
-    output.push_str("global _start\n");
-    output.push_str("_start:\n");
+    println!("section .text");
+    println!("extern printf");
+    println!("global _start");
+    println!("_start:");
 
     for instruction in &instructions {
         match instruction {
             | Instruction::PushAsm(line) => {
-                output.push_str("   ;; custom assembly\n");
-                output.push_str(line);
+                println!("   ;; custom assembly");
+                println!("{line}");
             }
             | Instruction::Push(value) => {
-                output.push_str(&format!("   ;; push(int) {value}\n"));
-                output.push_str(&format!("   mov rax, {value}\n"));
-                output.push_str("   push rax\n");
+                println!("   ;; push(int) {value}");
+                println!("   mov rax, {value}");
+                println!("   push rax");
             }
             | Instruction::BuiltInCall(fun) => match fun {
                 | BuiltIn::Puts => {
-                    output.push_str("   ;; puts integer\n");
-                    output.push_str("   pop rdi\n");
-                    output.push_str("   call print_integer\n");
+                    println!("   ;; puts integer");
+                    println!("   mov rdi, print_int_fmt");
+                    println!("   pop rsi");
+                    println!("   xor rax, rax");
+                    println!("   call printf");
                 }
                 | _ => unreachable!(),
             }
+            Instruction::Store(name) => {
+                println!("   ;; store variable {}", name);
+                println!("   pop rax");
+                println!("   mov [rsp - {}], rax", symbol_table[name]);
+            }
+            Instruction::Load(name) => {
+                println!("   ;; load variable {}", name);
+                println!("   mov rax, [rsp - {}]", symbol_table[name]);
+                println!("   push rax");
+            }
             | Instruction::BinOp(op) => match op {
                  | BinOp::Add => {
-                    output.push_str("   ;; add \n");
-                    output.push_str("   pop rax ;; rax <- RHS\n");
-                    output.push_str("   pop rbx ;; rbx <- LHS\n");
-                    output.push_str("   add rax, rbx\n");
-                    output.push_str("   push rax\n");
+                    println!("   ;; add ");
+                    println!("   pop rax ;; rax <- RHS");
+                    println!("   pop rbx ;; rbx <- LHS");
+                    println!("   add rax, rbx");
+                    println!("   push rax");
                 }
                 | BinOp::Mul => {
-                    output.push_str("   ;; mul \n");
-                    output.push_str("   pop rax ;; rax <- RHS\n");
-                    output.push_str("   pop rbx ;; rbx <- LHS\n");
-                    output.push_str("   mul rbx\n");
-                    output.push_str("   push rax\n");
+                    println!("   ;; mul ");
+                    println!("   pop rax ;; rax <- RHS");
+                    println!("   pop rbx ;; rbx <- LHS");
+                    println!("   mul rbx");
+                    println!("   push rax");
                 }
                 | BinOp::Sub => {
-                    output.push_str("   ;; sub \n");
-                    output.push_str("   pop rax ;; rax <- RHS\n");
-                    output.push_str("   pop rbx ;; rbx <- LHS\n");
-                    output.push_str("   sub rbx, rax\n");
-                    output.push_str("   push rbx\n");
+                    println!("   ;; sub ");
+                    println!("   pop rax ;; rax <- RHS");
+                    println!("   pop rbx ;; rbx <- LHS");
+                    println!("   sub rbx, rax");
+                    println!("   push rbx");
                 }
                 | _ => unreachable!()
             }
@@ -147,21 +137,20 @@ fn generate_asm(instructions: Vec<Instruction>) {
         }
     }
 
-    output.push_str("   mov rax, 60 ;; exit(\n");
-    output.push_str("   mov rdi, 0  ;;  0\n");
-    output.push_str("   syscall     ;; );\n");
+    println!("   mov rax, 60   ;; exit(");
+    println!("   xor rdi, rdi  ;;  0");
+    println!("   syscall       ;; );");
 
-    println!("{}", output);
+    println!("section .data");
+    println!("  print_int_fmt db \"%d\", 10")
 }
 
 
 fn main() {
     let file = std::env::args().nth(1).unwrap();
 
-    let is = parse_tokens(
+    parse_tokens(
         &mut lex_into_tokens(&std::fs::read_to_string(&file).unwrap(), &file),
         true,
     );
-
-    generate_asm(is);
 }
