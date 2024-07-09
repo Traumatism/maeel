3b66c5fa5b33aff42f9685e0dc4ecee478ec72e6
Victor <traumatism@users.noreply.github.com>
2024-07-09 14:16:51 +0200

    Grammar update

--- maeel.rs
+++ maeel.rs
@@ -1,10 +1,3 @@
-/*
-This code was made without the help of any language
-server and auto-formatter.
-
-(Qualité Artisale)
-*/
-
 use std::fs::File;
 use std::io::Read;
 
@@ -35,12 +28,12 @@ macro_rules! expect_stack {
 
 macro_rules! binop {
     ($app:expr, $self:expr, $file:expr, $line:expr) => {{
-        let out = $app(
-            $self.pop().unwrap_or_else(|_| { emit_error!($file, $line, "stack is empty! (binary operation LHS)") }),
-            $self.pop().unwrap_or_else(|_| { emit_error!($file, $line, "stack is empty! (binary operation RHS)") }),
+        let output = $app(
+            $self.pop().unwrap_or_else(|_| emit_error!($file, $line, "stack is empty! (binary operation LHS)")),
+            $self.pop().unwrap_or_else(|_| emit_error!($file, $line, "stack is empty! (binary operation RHS)")),
         );
 
-        $self.push(out)
+        $self.push(output)
     }};
 }
 
@@ -77,11 +70,11 @@ impl BocchiVM {
     fn process_tokens(
         &mut self,
         tokens: &mut Vec<TokenData>,
-        vars: &mut Mapper<Cord>,
-        funs: &mut Mapper<FunData>,
-        rev: bool,
+        variables: &mut Mapper<Cord>,
+        functions: &mut Mapper<FunData>,
+        reverse: bool,
     ) {
-        if rev /* Sometimes we might act like the tokens vec was a stack */ { tokens.reverse(); }
+        if reverse /* Sometimes we might act like the tokens vec was a stack */ { tokens.reverse(); }
 
         while let Some((token, file, line)) = tokens.pop() {
             match token {
@@ -99,36 +92,36 @@ impl BocchiVM {
                     self.push(Cord::Fun((block.as_slice().into(), true)))
                 }
                 | Token::Sym(M_FUN_PUSH!()) => {
-                    let fun_name /* Fun name */   = expect_token!(Name, tokens, file, line);
-                    let fun      /* Fun object */ = funs.get(&fun_name).unwrap_or_else(|| {
-                        emit_error!(file, line, format!("undefined function: {fun_name:?}"))
+                    let function_name /* function name */   = expect_token!(Name, tokens, file, line);
+                    let function      /* function object */ = functions.get(&function_name).unwrap_or_else(|| {
+                        emit_error!(file, line, format!("undefined function: {function_name:?}"))
                     });
 
-                    self.push(Cord::Fun(fun.clone()))
+                    self.push(Cord::Fun(function.clone()))
                 }
                 | Token::Sym(M_EXEC!()) /* Manually call a function */ => {
-                    let (fun_tokens, inline) = expect_stack!(Fun, self, file, line);
+                    let (function_tokens, inline) = expect_stack!(Fun, self, file, line);
 
                     match inline {
-                        | true  => fun_tokens.iter().for_each(|t| tokens.push(t.clone())),
+                        | true  => function_tokens.iter().for_each(|t| tokens.push(t.clone())),
                         | false => self.process_tokens(
-                            &mut fun_tokens.to_vec(), &mut vars.clone(), funs, true
+                            &mut function_tokens.to_vec(), &mut variables.clone(), functions, true
                         ),
                     }
                 }
                 | Token::Sym(M_THEN!()) /* Basically "if" statement */ => {
-                    let tmp_tokens = expect_token!(Block, tokens, file, line);
+                    let temp_tokens = expect_token!(Block, tokens, file, line);
 
                     if expect_stack!(Int, self, file, line) /* The boolean (actually an integer, anyways :3) */ == 1 {
-                        let tmp_tokens_len = tmp_tokens.len();
+                        let temp_tokens_length = temp_tokens.len();
 
-                        for idx in 0..(tmp_tokens_len) { /* Pushing from end to start */
-                            tokens.push(tmp_tokens.get(tmp_tokens_len - idx - 1).unwrap().clone())
+                        for index in 0..(temp_tokens_length) { /* Pushing from end to start */
+                            tokens.push(temp_tokens.get(temp_tokens_length - index - 1).unwrap().clone())
                         }
                     }
                 }
-                | Token::Sym('§') /* Can be pushed by interpreter only */ => {
-                    vars.insert(
+                | Token::Sym(M_FORCE_DEF!()) /* Can be pushed by interpreter only */ => {
+                    variables.insert(
                         expect_token!(Name, tokens, file, line),
                         self.pop().unwrap_or_else(|_| emit_error!(file, line, "stack is empty! (maeel)")),
                     );
@@ -136,67 +129,67 @@ impl BocchiVM {
                 | Token::Sym(M_DEF!()) /* Assign stack top value to next name */ => {
                     let name = expect_token!(Name, tokens, file, line);
                     if name.starts_with("__") /* Private field */ { panic!(/* TODO: make the error message */) }
-                    vars.insert(name, self.pop().unwrap_or_else(|_| emit_error!(file, line, "stack is empty!")));
+                    variables.insert(name, self.pop().unwrap_or_else(|_| emit_error!(file, line, "stack is empty!")));
                 }
                 | Token::Sym(char) => emit_error!(file, line, format!("unknown symbol: {char}.")),
                 | Token::Name(name) => match name.as_str() {
                     | M_PUTS!() => print!("{}", self.pop().unwrap() /* TODO: make an error message when stack is empty */),
                     | M_ELIST!() => self.push(Cord::Lst(Vec::new())),
                     | M_FUN!() => {
-                        let mut fun_name   = expect_token!(Name, tokens, file, line);
-                        let mut fun_tokens = Vec::default();
-                        let is_inline      = fun_name == M_INLINE!();
+                        let mut function_name   = expect_token!(Name, tokens, file, line);
+                        let mut function_tokens = Vec::default();
+                        let is_inline           = function_name == M_INLINE!();
 
-                        if is_inline { fun_name = expect_token!(Name, tokens, file, line) }
+                        if is_inline { function_name = expect_token!(Name, tokens, file, line) }
 
-                        while let Some(tmp_token) = tokens.pop() {
-                            match tmp_token.clone() {
-                                | (Token::Block(tmp_tokens), _, _) => {
-                                    fun_tokens.reverse(); /* uhm */
-                                    fun_tokens.extend(tmp_tokens);
-                                    fun_tokens.reverse(); /* never ask if maeel could be faster */
+                        while let Some(temp_token) = tokens.pop() {
+                            match temp_token.clone() {
+                                | (Token::Block(temp_tokens), _, _) => {
+                                    function_tokens.reverse(); /* uhm */
+                                    function_tokens.extend(temp_tokens);
+                                    function_tokens.reverse(); /* never ask if maeel could be faster */
                                     break; /* TODO: remove this break, f*ck breaks */
                                 }
                                 | (Token::Name(_), file, line) => {
-                                    fun_tokens.push(tmp_token);
-                                    fun_tokens.push((Token::Sym('§'), file, line));
+                                    function_tokens.push(temp_token);
+                                    function_tokens.push((Token::Sym('§'), file, line));
                                 }
                                 | (other, file, line) => {
                                     emit_error!(
                                         file,
                                         line,
-                                        format!("expected name(s) or a code block after 'fun {fun_name}'; got {other:?} instead.")
+                                        format!("expected name(s) or a code block after 'function {function_name}'; got {other:?} instead.")
                                     )
                                 }
                             }
                         }
 
-                        funs.insert(fun_name.clone(), (fun_tokens.as_slice().into(), is_inline));
+                        functions.insert(function_name.clone(), (function_tokens.as_slice().into(), is_inline));
                     }
                     | M_LEN!() => {
-                        let out = match self.pop() {
+                        let length = match self.pop() {
                             | Ok(Cord::Str(string)) => string.len(),
                             | Ok(Cord::Lst(xs))     => xs.len(),
                             | Ok(other)             => emit_error!(file, line, format!("expected string or list, got {other:?}")),
                             | Err(_)                => emit_error!(file, line, "expected string or list, got EOS."),
                         } as M_INT_SIZE;
 
-                        self.push(Cord::Int(out))
+                        self.push(Cord::Int(length))
                     }
                     | M_GET!() => {
-                        let idx = expect_stack!(Int, self, file, line) as usize;
+                        let index = expect_stack!(Int, self, file, line) as usize;
 
                         match self.pop() {
                             | Ok(Cord::Lst(xs)) => self.push(
-                                xs.get(idx)
-                                    .unwrap_or_else(|| {emit_error!(file, line, format!("unknown idx: {idx}"))})
+                                xs.get(index)
+                                    .unwrap_or_else(|| emit_error!(file, line, format!("unknown index: {index}")))
                                     .clone(),
                             ),
                             | Ok(Cord::Str(string)) => self.push(Cord::Str(
                                 string
                                     .chars()
-                                    .nth(idx)
-                                    .unwrap_or_else(|| {emit_error!(file, line, format!("unknown idx: {idx}"))})
+                                    .nth(index)
+                                    .unwrap_or_else(|| emit_error!(file, line, format!("unknown index: {index}")))
                                     .to_string(),
                             )),
                             | Ok(other) => emit_error!(file, line, format!("unindexable: {other:?}")),
@@ -204,15 +197,15 @@ impl BocchiVM {
                         }
                     }
                     | M_READ!() => {
-                        let buf_size = expect_stack!(Int, self, file, line);
-                        let mut buf  = vec![0u8; buf_size as usize];
+                        let buffer_size = expect_stack!(Int, self, file, line);
+                        let mut buffer  = vec![0u8; buffer_size as usize];
 
-                        File::open(expect_stack!(Str, self, file, line)) /* Copy `buf_size` bytes from file to `buf` */
+                        File::open(expect_stack!(Str, self, file, line)) /* Copy `buffer_size` bytes from file to `buf` */
                             .unwrap()
-                            .read_exact(&mut buf)
+                            .read_exact(&mut buffer)
                             .unwrap();
 
-                        let content_bytes = buf /*  Convert 8 bytes size integers to M_INT_SIZE integers */
+                        let content_bytes = buffer /*  Convert 8 bytes size integers to M_INT_SIZE integers */
                             .iter()
                             .map(|byte| Cord::Int(*byte as M_INT_SIZE))
                             .collect();
@@ -229,21 +222,21 @@ impl BocchiVM {
                             }),
                         };
 
-                        let tmp_tokens     = lex_into_tokens(&content, &target);
-                        let tmp_tokens_len = tmp_tokens.len();
+                        let temp_tokens     = lex_into_tokens(&content, &target);
+                        let temp_tokens_length = temp_tokens.len();
 
                         /* Push tokens from end to start (reverse) */
-                        for idx in 0..tmp_tokens_len {
-                            tokens.push(tmp_tokens.get(tmp_tokens_len - idx - 1).unwrap().clone())
+                        for index in 0..temp_tokens_length {
+                            tokens.push(temp_tokens.get(temp_tokens_length - index - 1).unwrap().clone())
                         }
                     }
                     | name => {
-                        if let Some(value) = vars.get(name) {
+                        if let Some(value) = variables.get(name) {
                             self.push(value.clone())
-                        } else if let Some((fun_tokens, inline)) = funs.get(name) {
+                        } else if let Some((function_tokens, inline)) = functions.get(name) {
                             match inline {
-                                | true  => fun_tokens.iter().for_each(|t| tokens.push(t.clone())),
-                                | false => self.process_tokens(&mut fun_tokens.to_vec(), &mut vars.clone(), funs, false),
+                                | true  => function_tokens.iter().for_each(|t| tokens.push(t.clone())),
+                                | false => self.process_tokens(&mut function_tokens.to_vec(), &mut variables.clone(), functions, false),
                             }
                         } else {
                             emit_error!(file, line, format!("unknown name {name}"))
@@ -256,13 +249,13 @@ impl BocchiVM {
 
     /* Push an object to the stack */
     fn push(&mut self, value: Cord) {
-        let future_h = Guitar::new(value);
+        let future_head = Guitar::new(value);
 
         if !self.head.is_null() {
-            unsafe { (*future_h).next = self.head; }
+            unsafe { (*future_head).next = self.head; }
         }
 
-        self.head = future_h;
+        self.head = future_head;
     }
 
     /* Drop-and-return the stack head */
@@ -270,9 +263,9 @@ impl BocchiVM {
         match self.head.is_null() {
             | true  => Err("Stack is empty".into()),
             | false => {
-                let current_h = unsafe { Box::from_raw(self.head) };
-                self.head = current_h.next;
-                Ok(current_h.value)
+                let current_head = unsafe { Box::from_raw(self.head) };
+                self.head = current_head.next;
+                Ok(current_head.value)
             }
         }
     }
@@ -397,7 +390,7 @@ fn main() {
     .process_tokens(
         &mut lex_into_tokens(&std::fs::read_to_string(&file).unwrap(), &file),
         &mut std::collections::HashMap::default(), /* Variables Hashmap */
-        &mut std::collections::HashMap::default(), /* Funs Hashmap */
+        &mut std::collections::HashMap::default(), /* functions Hashmap */
         true,
     )
 }
